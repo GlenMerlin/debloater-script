119d1762189bc6e00e72bbeaa553bad0c6193371
GlenMerlin <Glenmerlin@protonmail.com>
2021-09-08 18:21:18 -0700

    simplification

--- debloater.bat
+++ debloater.bat
@@ -1,3 +1,8 @@
-cd \Downloads\Windows10Debloater-master\Windows10Debloater-master
+cd C:\Users\%USERNAME%\Downloads\Windows10Debloater-master\Windows10Debloater-master
+Powershell.exe -File script-dl.ps1
 Powershell.exe -executionpolicy unrestricted -File Windows10Debloater.ps1
 Powershell.exe -executionpolicy default
+del script-dl.ps1
+del Windows10Debloater.ps1
+del README.md
+@REM del debloater.bat
---
+++ script-dl.ps1
@@ -0,0 +1,1 @@
+Invoke-WebRequest "https://raw.githubusercontent.com/Sycnex/Windows10Debloater/master/Windows10Debloater.ps1" -OutFile "C:\Users\%USERNAME%\Downloads\Windows10Debloater.ps1"
