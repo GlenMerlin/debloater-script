92e0f5a9b9fa4c4d925c3575209b4d05e2f0cb80
GlenMerlin <GlenMerlin@protonmail.com>
2021-09-13 12:09:10 -0700

    fix deleting README.txt instead of .md

--- debloater.bat
+++ debloater.bat
@@ -3,5 +3,5 @@ Powershell.exe -ExecutionPolicy Bypass -File script-dl.ps1
 Powershell.exe -WindowStyle Hidden -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File "Windows10Debloater.ps1"' -Verb RunAs}"
 del script-dl.ps1
 del Windows10Debloater.ps1
-del README.md
+del README.txt
 del debloater.bat
