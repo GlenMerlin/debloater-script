65c1e5521347906e3c00d4ec7c888606cf13e590
GlenMerlin <GlenMerlin@protonmail.com>
2021-09-13 12:07:51 -0700

    Properly Request Admin Perms when required

--- debloater.bat
+++ debloater.bat
@@ -1,6 +1,6 @@
 cd C:\Users\%USERNAME%\Downloads\
-Powershell.exe -executionpolicy bypass -File script-dl.ps1
-Powershell.exe -File Windows10Debloater.ps1
+Powershell.exe -ExecutionPolicy Bypass -File script-dl.ps1
+Powershell.exe -WindowStyle Hidden -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File "Windows10Debloater.ps1"' -Verb RunAs}"
 del script-dl.ps1
 del Windows10Debloater.ps1
 del README.md
