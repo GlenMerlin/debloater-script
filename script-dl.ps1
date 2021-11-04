430581a486edcefe28c797c5c5c6a37d8a56c0b7
GlenMerlin <GlenMerlin@protonmail.com>
2021-11-04 13:33:42 -0700

    Updated to use my fork of the debloater script for better reliability

--- script-dl.ps1
+++ script-dl.ps1
@@ -1,1 +1,1 @@
-Invoke-WebRequest "https://raw.githubusercontent.com/Sycnex/Windows10Debloater/master/Windows10Debloater.ps1" -OutFile "Windows10Debloater.ps1"
+Invoke-WebRequest "https://raw.githubusercontent.com/GlenMerlin/Windows10Debloater/master/Windows10Debloater.ps1" -OutFile "Windows10Debloater.ps1"
